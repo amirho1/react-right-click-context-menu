91a8c8520998b69da30a3609faeee55b3471dd40
amirho1 <amirhosseinsalighedar@gmail.com>
2021-11-21 00:36:38 +0330

    write some tests for  RightClickMenu

--- src/RightClickMenu/RightClickMenu.test.tsx
+++ src/RightClickMenu/RightClickMenu.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { screen, cleanup, render } from "@testing-library/react";
+import { screen, cleanup, render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import RightClickMenu from "./RightClickMenu";
 
@@ -10,12 +10,35 @@ describe("RightClickMenu", () => {
     cleanup();
 
     // render component
-    render(<RightClickMenu />);
+    const page = (
+      <div className="test" data-testid="test">
+        <RightClickMenu
+          rightClickTargets={[
+            { className: "test", menuList: ["hello", "world"] },
+          ]}
+        />
+      </div>
+    );
+    render(page);
 
     element = screen.getByTestId("RightClickMenu");
   });
 
   it("should be in the documentation", () => {
     expect(element).toBeInTheDocument();
+    expect(element.tagName).toBe("UL");
+  });
+
+  it("shouldn't have the display className or not be visible", () => {
+    expect(element.className).not.toMatch("display");
+  });
+
+  it("should show menu on page on the right click on targetClass", () => {
+    const testElement = screen.queryByTestId("test");
+
+    if (testElement) {
+      fireEvent.contextMenu(testElement);
+    }
+    expect(element.className).toMatch("display");
   });
 });
