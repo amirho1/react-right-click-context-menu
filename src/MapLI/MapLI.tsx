4fb85ef86df6baaf8075a40996e7598e6d167d90
amirho1 <amirhosseinsalighedar@gmail.com>
2021-11-21 17:05:11 +0330

    map list item and have some defualt style

---
+++ src/MapLI/MapLI.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement } from "react";
+import "./MapLI.scss";
+import classNames from "classnames";
+
+interface Props {
+  menuList: (JSX.Element | string)[];
+  liClassName?: string;
+}
+
+export default function MapListOfElements({ menuList, liClassName }: Props) {
+  return (
+    <>
+      {menuList.map((item, index) => {
+        return (
+          <li key={index} className={classNames("list-item", liClassName)}>
+            {item}
+          </li>
+        );
+      })}
+    </>
+  );
+}
